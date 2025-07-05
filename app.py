4859ae2685f095b02aff6736a06957b0fc23efd2
Carlini <chiipanexcar@gmail.com>
2025-07-05 10:45:04 -0400

    Probando

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+  
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "¡Hola desde Flask!"
 
 @app.route('/curriculum')
 def resume():
