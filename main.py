e604c17df720c3f21f211afa2e572a4795ca272a
Carlini <chiipanexcar@gmail.com>
2025-07-05 09:49:06 -0400

    Modificado en name

--- main.py
+++ main.py
@@ -17,5 +17,9 @@ def projects():
 def contact():
     return render_template('contacto.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
